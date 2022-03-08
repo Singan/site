7a758f1c88bf49bcf3b2cb53c7074c02feca5a5d
석성희 <sunghee119z@naver.com>
2022-03-08 23:24:07 +0900

    bd
    bd

--- src/main/java/com/spring/site/web/BoardController.java
+++ src/main/java/com/spring/site/web/BoardController.java
@@ -1,7 +1,18 @@
 package com.spring.site.web;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 @Controller
+@RequestMapping("/templates/board")
 public class BoardController {
+    @GetMapping("/")
+    public String main(HttpServletRequest request, HttpServletResponse response) {
+
+        return "home";
+    }
 }
