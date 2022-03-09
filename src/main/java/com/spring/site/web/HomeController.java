6133757f54c77956edff0aa86e16b438f808dd06
js988174 <76925402+js988174@users.noreply.github.com>
2022-03-09 23:06:28 +0900

    token 완성

--- src/main/java/com/spring/site/web/HomeController.java
+++ src/main/java/com/spring/site/web/HomeController.java
@@ -1,5 +1,6 @@
 package com.spring.site.web;
 
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -22,6 +23,7 @@ public class HomeController {
     @ResponseBody
     public String tokenCheck(){
         System.out.println("토큰체크 컨트롤러");
+        System.out.println(SecurityContextHolder.getContext().getAuthentication().getPrincipal());
         return "home";
     }
 }
--- src/main/java/com/spring/site/web/MemberController.java
+++ src/main/java/com/spring/site/web/MemberController.java
@@ -2,8 +2,14 @@ package com.spring.site.web;
 
 
 import com.spring.site.domain.Member;
+import com.spring.site.etc.LoginSecurityService;
 import com.spring.site.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -14,6 +20,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import javax.servlet.ServletContext;
 import javax.validation.Valid;
+import java.security.Principal;
 import java.util.List;
 
 
@@ -34,5 +41,25 @@ public class MemberController {
         return "/member/memberList";
     }
 
+    @GetMapping("/myPage")
+        public String myPage(Model model, @AuthenticationPrincipal Principal principal) {
+        Member member = new Member();
+//        member.setName(principal.getName());
+        model.addAttribute("member", member.getId());
+        model.addAttribute("member", member);
+
+            return "member/myPage";
+        }
+
+//    @PostMapping("/myPage")
+//    public String userEdit(Member form, BindingResult result, Member currentMember) {
+//        if(result.hasErrors()) {
+//            return "redirect:/member/myPage";
+//        }
+//
+//        currentMember.setPw(form.getPw());
+//        currentMember.setName(form.getName());
+//        return "redirect:/member/myPage";
+//    }
 
 }
