bf126b8b29b40c915d162bc25e809bb67b475810
석성희 <sunghee119z@naver.com>
2022-03-11 22:34:48 +0900

    페이징
    완성

--- src/main/java/com/spring/site/mapper/BoardMapper.java
+++ src/main/java/com/spring/site/mapper/BoardMapper.java
@@ -2,6 +2,7 @@ package com.spring.site.mapper;
 
 
 import com.spring.site.domain.Board;
+import com.spring.site.domain.Criteria;
 import com.spring.site.domain.Member;
 import org.apache.ibatis.annotations.*;
 
@@ -11,15 +12,17 @@ import java.util.List;
 @Mapper
 public interface BoardMapper {
 
-    String listSelect = "select no,writer,date,title from board ";
+    String listSelect = "select * from board order by no ";
     @Options(useGeneratedKeys = true, keyProperty = "no")
     @Insert("INSERT INTO board(title,content,writer,date,file) VALUES(#{title}, #{content},#{writer},#{date},#{file})")
     int insert(Board board) throws Exception;
-    @Select(listSelect)
-    List<Board> list() throws Exception;
+    @Select(listSelect + " limit ${recordsStart},${recordsPerPage}")
+    List<Board> list(Criteria criteria) throws Exception;
     @Select(listSelect+"where = #{id}")
     List<Board> searchList(Member member);
     @Select("SELECT * FROM board where no = #{no}")
     Board detailSearch(int no);
 
+    @Select("SELECT count(*) FROM board")
+    int countBoard();
 }
--- src/main/java/com/spring/site/web/BoardController.java
+++ src/main/java/com/spring/site/web/BoardController.java
@@ -1,6 +1,7 @@
 package com.spring.site.web;
 
 import com.spring.site.domain.Board;
+import com.spring.site.domain.Criteria;
 import com.spring.site.domain.Member;
 import com.spring.site.etc.LoginSecurity;
 import com.spring.site.service.BoardService;
@@ -13,6 +14,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -29,11 +31,14 @@ public class BoardController {
     BoardService boardService;
     String dir = "/board";
     @GetMapping("/list")
-    public String list(Model model) throws Exception  {
-        List<Board> bl = boardService.list();
+    public String list(Model model,@RequestParam(defaultValue = "1") int no) throws Exception  {
+        Criteria criteria = new Criteria(no);
+        criteria.setCurrentPageNo(no);
+        List<Board> bl = boardService.list(criteria);
         model.addAttribute("list",bl);
         System.out.println("보드 리스트 확인");
-
+        int boardCount = (boardService.countBoard()+9)/criteria.getRecordsPerPage();
+        model.addAttribute("boardCount",boardCount);
         return "/board/list";
     }
     // board writer , title,file , date,content
