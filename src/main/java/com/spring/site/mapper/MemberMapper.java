d472bcc6a71b5f01d0c3a520cb460012ceeb0729
석성희 <sunghee119z@naver.com>
2022-03-17 21:34:39 +0900

    aa
    aa

--- src/main/java/com/spring/site/mapper/MemberMapper.java
+++ src/main/java/com/spring/site/mapper/MemberMapper.java
@@ -11,7 +11,7 @@ import java.util.List;
 public interface MemberMapper {
 
     @Options(useGeneratedKeys = true, keyProperty = "no")
-    @Insert("INSERT INTO  member(id, pw, name) VALUES(#{id}, #{pw},#{name})")
+    @Insert("INSERT INTO  member(id, pw, name,phone,email,rDay,profile) VALUES(#{id}, #{pw},#{name},#{phone},#{email},now(),#{profile})")
     int insert(Member member) throws Exception;
     @Select("SELECT * FROM member")
     List<Member> allList() throws Exception;
